fb0fc3daa41541fbff067079bded26d90e9407d1
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-09-01 17:45:06 -0700

    newPm

--- excercises-2.js
+++ excercises-2.js
@@ -1,42 +1,61 @@
 // ---------------------
-// Define a function max() that takes two numbers as 
-// arguments and returns the largest of them. Use the 
+// Define a function max() that takes two numbers as
+// arguments and returns the largest of them. Use the
 // if-then-else construct available in JavaScript.
 // ---------------------
+function max(a, b) {
+  if (a > b) {
+    return a
+  } else {
+    return b
+  }
+}
 
-console.assert(max(2,3) === 3)
-console.assert(max(23,24) === 24)
-console.assert(max(-23, 12) === 12)
+console.log(max(2, 3))
+console.log(max(23, 24))
+console.log(max(-23, 12))
 
 // ---------------------
-// Define a function maxOfThree() that takes three 
+// Define a function maxOfThree() that takes three
 // numbers as arguments and returns the largest of them.
 // ---------------------
 
-console.assert(maxOfThree(2,56,3) === 56)
-console.assert(maxOfThree(12,3,4) === 12)
-console.assert(maxOfThree(-12,4,-5) === 4)
+function maxOfThree(a, b, c) {
+  return Math.max(a, b, c)
+}
+
+console.log(maxOfThree(2, 56, 3))
+console.log(maxOfThree(12, 3, 4))
+console.log(maxOfThree(-12, 4, -5))
 
 // ---------------------
-// Write a function isVowel() that takes a character (i.e. a 
-// string of length 1) and returns true if it is a 
+// Write a function isVowel() that takes a character (i.e. a
+// string of length 1) and returns true if it is a
 // vowel, false otherwise.
 // ---------------------
+function isVowel(a) {
+  if (a == "a" || a == "e" || a == "i" || a == "o" || a == "u" || a == "E") {
+    return true
+  } else {
+    return false
+  }
+}
 
-console.assert(isVowel(0) === false);
-console.assert(isVowel("B") === false);
-console.assert(isVowel("b") === false);
-console.assert(isVowel("a") === true);
-console.assert(isVowel("E") === true);
-console.assert(isVowel("2") === false)
+console.log(isVowel(0))
+console.log(isVowel("B"))
+console.log(isVowel("b"))
+console.log(isVowel("a"))
+console.log(isVowel("E"))
+console.log(isVowel("2"))
 
 // ---------------------
-// Write a function rovarspraket() that will translate 
-// a text into "Rovarspraket". That is, double every 
-// consonant and place an occurrence of "o" in between. 
-// For example, translate("this is fun") should return 
+// Write a function rovarspraket() that will translate
+// a text into "Rovarspraket". That is, double every
+// consonant and place an occurrence of "o" in between.
+// For example, translate("this is fun") should return
 // the string "tothohisos isos fofunon".
 // ---------------------
+function rovarspraket() {}
 
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
@@ -44,13 +63,17 @@ console.assert(rovarspraket("cat") === "cocatot")
 console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
 console.assert(rovarspraket(0) === "0")
 
-
 // ---------------------
-// Define a function reverse() that computes the reversal 
-// of a string. For example, reverse("jag testar") should 
+// Define a function reverse() that computes the reversal
+// of a string. For example, reverse("jag testar") should
 // return the string "ratset gaj".
 // ---------------------
-
-
-console.assert(reverse("books") === "skoob")
-console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
+function reverse(a) {
+  let reversed = ""
+  for (let i = a.length - 1; i >= 0; i--) {
+    reversed += a[i]
+  }
+  return reversed
+}
+console.log(reverse("books"))
+console.log(reverse("we don't want no trouble"))
